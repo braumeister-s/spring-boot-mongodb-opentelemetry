424e18e65c628ca0cbd1b9a3816a6c245b502a24
Bacher, Stefan <Stefan.Bacher+datev@DATEV.DE>
2025-08-15 09:01:25 +0200

    adds first simple Demoapplication

---
+++ src/main/java/com/example/demo/Note.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@NoArgsConstructor
+@RequiredArgsConstructor
+@Document(collection = "notes")
+public class Note {
+    @Id
+    private String id;
+    @NonNull
+    private String title;
+    private String content;
+}
